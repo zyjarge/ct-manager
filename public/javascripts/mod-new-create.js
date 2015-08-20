11fe0e312e2589885e7c7a6ff3dcc21a5592d626
zhangyong <zhangyong@fun.tv>
2015-08-20 19:14:05 +0800

    commit mod-new-crearte.js

---
+++ public/javascripts/mod-new-create.js
@@ -0,0 +1,13 @@
+/**
+ * Created by zhangyong on 2015/8/19.
+ */
+
+var ctl_create_new = function($scope){
+    alert('111');
+    $scope.selectedType="order";
+}
+
+
+var ctlTest = function($scope){
+    alert('222');
+}
