7e5f7a85a602e0db2fe6a995d3d2986d2d0971a1
zhangyong <zhangyong@funshion.com>
2015-08-21 10:34:41 +0800

    订单和广告主通过按钮进行切换

--- public/javascripts/mod-new-create.js
+++ public/javascripts/mod-new-create.js
@@ -2,12 +2,42 @@
  * Created by zhangyong on 2015/8/19.
  */
 
-var ctl_create_new = function($scope){
-    alert('111');
+
+var app = angular.module('myApp', []);
+
+app.controller('ctl_create_new', function($scope) {
     $scope.selectedType="order";
-}
 
+    $scope.setOrder = function(){
+        $scope.selectedType="order";
+    }
 
-var ctlTest = function($scope){
-    alert('222');
-}
+    $scope.setAdvertiser = function(){
+        $scope.selectedType="advertiser";
+    }
+
+    $scope.getOrderCssClass= function () {
+        //return{
+        //    btn-primary: $scope.selectedType=="order",
+        //    btn-default: $scope.selectedType!="order"
+        //};
+        if($scope.selectedType==="order"){return "btn-primary";}
+        return "btn-default";
+    }
+
+    $scope.getAdvertiserCssClass = function () {
+        if($scope.selectedType==="advertiser"){return "btn-primary";}
+        return "btn-default";
+    }
+
+    $scope.getPlaceholder = function () {
+        if($scope.selectedType==="order"){
+            return "订单的名称";
+        }
+
+        if($scope.selectedType==="advertiser"){
+            return "广告主的名称";
+        }
+    }
+    
+});
