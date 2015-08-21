ca5e21ddf09ed5fbc2b4db0f716a7d119edfe348
zhangyong <zhangyong@fun.tv>
2015-08-21 16:07:08 +0800

    commit mysql dependency.

--- public/javascripts/mod-new-create.js
+++ public/javascripts/mod-new-create.js
@@ -2,42 +2,53 @@
  * Created by zhangyong on 2015/8/19.
  */
 
-
 var app = angular.module('myApp', []);
 
-app.controller('ctl_create_new', function($scope) {
-    $scope.selectedType="order";
+app.controller('ctl_create_new', function ($scope) {
 
-    $scope.setOrder = function(){
-        $scope.selectedType="order";
-    }
+    $scope.contentTracking = {
+        selectedType: "order",
+        startDate: "",
+        endDate: ""
+    };
 
-    $scope.setAdvertiser = function(){
-        $scope.selectedType="advertiser";
-    }
+    $scope.getList = function () {
 
-    $scope.getOrderCssClass= function () {
-        //return{
-        //    btn-primary: $scope.selectedType=="order",
-        //    btn-default: $scope.selectedType!="order"
-        //};
-        if($scope.selectedType==="order"){return "btn-primary";}
+    };
+
+    $scope.setOrder = function () {
+        $scope.contentTracking.selectedType = "order";
+    };
+
+    $scope.setAdvertiser = function () {
+        $scope.contentTracking.selectedType = "advertiser";
+    };
+
+    $scope.getOrderCssClass = function () {
+        if ($scope.contentTracking.selectedType === "order") {
+            return "btn-success";
+        }
         return "btn-default";
-    }
+    };
 
     $scope.getAdvertiserCssClass = function () {
-        if($scope.selectedType==="advertiser"){return "btn-primary";}
+        if ($scope.contentTracking.selectedType === "advertiser") {
+            return "btn-success";
+        }
         return "btn-default";
-    }
+    };
 
     $scope.getPlaceholder = function () {
-        if($scope.selectedType==="order"){
+        if ($scope.contentTracking.selectedType === "order") {
             return "订单的名称";
         }
 
-        if($scope.selectedType==="advertiser"){
+        if ($scope.contentTracking.selectedType === "advertiser") {
             return "广告主的名称";
         }
-    }
-    
+    };
+
+    $scope.submit_form = function () {
+        console.log("submit");
+    };
 });
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Content-Tracker Manager' });
 });
 
+router.get('/getAllRecords', function (req, res, next) {
+    var connection = mysql.createConnection({
+        host     : '192.168.16.241',
+        user     : 'root',
+        password : 'microlens',
+        database : 'microlens_master'
+    });
+
+    connection.connect(function(err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+        console.log('connected as id ' + connection.threadId);
+    });
+
+
+    connection.query('select * from content_tracking_base order by id desc', function(err, rows, fields) {
+        if (err) throw err;
+    });
+
+    connection.end();
+});
 module.exports = router;
